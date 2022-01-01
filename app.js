5b03496fe65da778f51086eeb0b2d60a22cc60c9
Mohamed <qotozer@gmail.com>
2022-01-02 01:33:42 +0200

    installing mongoose and connect it with the mongodb atlas

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const placesRouter = require('./routes/places-routes');
+dotenv.config();
 const app = express();
 
 app.use('/api/places/', placesRouter);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
-  console.log(`The server is running on port ${PORT}`);
-});
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    app.listen(PORT, () => {
+      console.log(`The server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+connect();
