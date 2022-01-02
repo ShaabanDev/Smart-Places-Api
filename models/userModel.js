5cd99de0149d8bfcd25797563ae545379f7e1bf1
Mohamed <qotozer@gmail.com>
2022-01-02 02:04:45 +0200

    creating the user model

---
+++ models/userModel.js
@@ -0,0 +1,30 @@
+const { model, Schema } = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  Image: {
+    type: String,
+    required: true,
+  },
+  places: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = model('User', userSchema);
