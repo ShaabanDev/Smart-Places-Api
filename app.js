77ba7dc806159379653245d7e7677b343aaa4f67
Mohamed <qotozer@gmail.com>
2022-01-13 20:15:44 +0200

    handling unsupported routes

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const placesRouter = require('./routes/places-routes');
+const HttpError = require('./models/http-error');
 dotenv.config();
 const app = express();
 
 app.use(express.json());
 app.use('/api/places/', placesRouter);
 
+app.use((req, res, next) => {
+  return next(new HttpError('unsupported request', 501))
+})
 app.use((error, req, res, next) => {
   if (res.headerSent) {
+    console.log(res.headerSent)
     return next(error);
   }
   res.status(error.code || 500);
