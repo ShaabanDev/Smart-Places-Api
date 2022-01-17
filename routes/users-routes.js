40eb017884fe768d54a4edacfeb591adedbddd97
Mohamed <qotozer@gmail.com>
2022-01-18 01:34:51 +0200

    adding file upload middleware to the sign up route to upload the image of the user

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,14 +1,12 @@
 const usersRoutes = require('express').Router();
 const { check } = require('express-validator');
 const { signup, login } = require('../controllers/user-controllers');
+const fileUpload = require('../middleware/file-upload');
 
 usersRoutes.post(
   '/signup',
-  [
-    check('name').not().isEmpty(),
-    check('email').normalizeEmail().isEmail(),
-    check('password').isLength({ min: 6 }),
-  ],
+  fileUpload.single('image'),
+
   signup
 );
 usersRoutes.post('/login', login);
